92fb4db9dcdbb3f1c89d36c5ef5f9314013f066d
Steven Meads <sbmeads@gmail.com>
2017-03-28 10:47:16 -0700

    setup webpack and react config

---
+++ client/components/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+     <div style={{textAlign: 'center'}}>
+        <h1>Food Hunt</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
